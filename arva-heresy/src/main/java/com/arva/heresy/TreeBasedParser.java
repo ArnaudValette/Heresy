ccd32f87ebc5e94018043dfad0b1c84a8cebcc7d
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-29 17:39:30 +0100

    comment TreeBasedParser.treeParse

--- arva-heresy/src/main/java/com/arva/heresy/TreeBasedParser.java
+++ arva-heresy/src/main/java/com/arva/heresy/TreeBasedParser.java
@@ -18,11 +18,54 @@ public class TreeBasedParser {
         this.commands = map;
     }
 
-    public void parse(String s, int lineNumber, TreeBasedParserState state, pNodes<? extends pNode> nodes) {
+    public void treeParse(String s, int lineNumber, TreeBasedParserState state, pNodes<? extends pNode> nodes) {
+        /*
+          the current treeParse implementation handles BracketParsing well
+          the idea is to have
+          A: A hashMap <Key, Function> and
+          B: a recursive hashMap <Key, HashMap<Key, ...<..., Tail>>>
+          C: Tails, Strings that serve to type and name the path taken through
+          the branches of B.
+        
+          For each character X, we use each key K from the current depth in B,
+          to call every function in A -> boolean A[K](X)
+        
+          if this is true, then we have a match and we must travel deeper in the forest
+          B, accessing B[K].
+        
+          if this is false, then we set B back to the initial state, and continue this
+          operation.
+        
+          Eventually, we will reach one end of the forest and from this we should
+          perform operations to handle our newly found expression.
+          e.g. the path to a footNote is '['->'f'->'n'->':'->'any'->']'.
+          Note that some keys of B triggers some /redundant/ functions such as 'any',
+          that allows for repetition ( any can lead, theorically, to n number of any).
+        
+          That's why state.handleRedundant(char, int, Commands) is run
+          before anything in our loop. The state keeps track of isRedundant and
+          previousCommandKey variables that allows us to perform such a thing easily.
+
+          previousCommandKey is set only if the previousCommand is redundant.
+
+          We need to rework this so it can handle both
+
+          1. the parsing for multiple expressions in a string that matches a
+          set of different expressions (that's what it does right now)
+
+          2. the parsing of expressions only at the start of the string,
+          making the line a HornNode according to the specification given
+          by HornNodeConfig. This is different because in this behavior,
+          any unmatched character should stop the loop and create a 
+          HornNode of paragraph type. Also, at any time a Tail is reached,
+          we should end parsing and commit our new HornNode of type specified
+          by the Tail object reached.
+
+         */
         state.reset(config);
         for (int i = 0, j = s.length(); i < j; i = i + 1) {
             char c = s.charAt(i);
-            state.handleRedundant(c,i,commands);
+            state.handleRedundant(c, i, commands);
             if (!state.hasMatched) {
                 List<String> currentCommandKeys = state.getCurrentCommandKeys();
                 Iterator<String> iterator = currentCommandKeys.iterator();
@@ -45,8 +88,6 @@ public class TreeBasedParser {
                 nodes.finalize(j);
             }
         }
-
-        //return new BracketResult(brackets, lineNumber);
     }
 
     public boolean itDoesMatch(String key, char c) {
