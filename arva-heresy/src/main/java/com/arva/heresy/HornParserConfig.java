d1cccb4f2a2fda205143fae8c1939f360c7fa122
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-29 13:16:14 +0100

    add reference to HORN regexp system

--- arva-heresy/src/main/java/com/arva/heresy/HornParserConfig.java
+++ arva-heresy/src/main/java/com/arva/heresy/HornParserConfig.java
@@ -6,6 +6,25 @@ public class HornParserConfig{
             {"[,f,n,:,any,], ", "footNote"},
             {"#,+,b,e,g,i,n,_", "startDirective"},
             {"#,+,e,n,d,_", "endDirective"},
-        }
+            /*
+              For reference, this is the regexp HORN-ts uses 
+                heading: /^\*+\s/,
+                list: /^\s*-\s/,
+                nList: /^\s*([A-Za-z]|[0-9]+)\.\s/,
+                sTemplate: /^#\+begin_/,
+                sTemplateEnd: /^#\+end_/,
+                //bSrc: /^#\+begin_src/,
+                //eSrc: /^#\+end_src/,
+                nSrc: /^#\+name:/,
+                table: /^\|([^\|]*\|)+$/,
+                tableSep: /^\|(-+\+)+-+\|$/,
+                empty: /^\s*$/,
+                HR: /^\s*-{5,}\s*$/,
+                orgCode: /^:\s/,
+                footNote: /^\[fn:\d+\]\s/,
+                paragraph: /.+/,
+                clock: /^CLOCK:\s/,
+            */
+        };
     }
 }
