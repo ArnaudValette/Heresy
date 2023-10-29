3f21ee1e7e76e4423cb125c72aee7532227eab3b
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-29 12:28:07 +0100

    business logic from app -> parser

--- arva-heresy/src/main/java/com/arva/heresy/App.java
+++ arva-heresy/src/main/java/com/arva/heresy/App.java
@@ -1,5 +1,6 @@
 package com.arva.heresy;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,24 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
-            // FileProcessor.iterativeCall("/home/truite/journal/journal.org", bracketParser::parse);
-            BracketParser bracketParser = new BracketParser(new ParserConfig()) ;
-            FormatParser formatParser = new FormatParser(new FormatParserConfig());
-            String teststr = "[[x][y]]*/ok/* [[file][file]] /*y_es*/ [[img.png]] **_ ok*[[a]]";
-            /*
-              First, parse Brackets from a line
-             */
-            BracketResult resultBracket = bracketParser.parse(teststr, 1);
-            /*
-              Brackets gives us limits that are unformatted,
-              for each of these limits, our bracketParser should parse
-              a substring of our line.
-             */
-            resultBracket.toBeFormatted().forEach(lim -> {
-                     String toProcess = teststr.substring(lim.get(0), lim.get(1));
-                     FormatResult fres = formatParser.parse(toProcess, lim);
-                     fres.formats.describe();
-             });
+        Parser parser = new Parser();
+        parser.parse(new File("/home/truite/journal/journal.org"));
+        //FileProcessor.iterativeCall("/home/truite/journal/journal.org", bracketParser::parse);
             
 
 
--- arva-heresy/src/main/java/com/arva/heresy/Parser.java
+++ arva-heresy/src/main/java/com/arva/heresy/Parser.java
@@ -1,5 +1,13 @@
 package com.arva.heresy;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Parser {
     /* We should operate from there to spawn Threads
@@ -25,7 +33,31 @@ public class Parser {
             }
        }
     */
-    public Parser() {
+    public void parse(File file) {
+        BracketParser b = new BracketParser(new ParserConfig());
+        FormatParser f = new FormatParser(new FormatParserConfig());
+        List<BracketResult> bRes = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            AtomicInteger ln = new AtomicInteger(1);
+            while((line = reader.readLine()) != null){
+                final String lineToProcess = line;
+                final int currLn = ln.getAndIncrement();
+                BracketResult bracketResult = b.parse(lineToProcess, currLn);
+                bRes.add(bracketResult);
+                bracketResult.toBeFormatted().forEach(lim ->{
+                        String toFormat = lineToProcess.substring(lim.get(0), lim.get(1));
+                        FormatResult formatResult = f.parse(toFormat, lim);
+                        formatResult.formats.describe();
+                    });
+            }
+        }
+        catch(FileNotFoundException e){
+            System.err.println("File " + file.toString() + " not found");
+        }
+        catch(IOException e){
+            System.err.println(e);
+        }
     }
 }
 
