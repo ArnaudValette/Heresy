676e3e262f2f6daf447ced40013c06196f4892ec
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-29 15:08:00 +0100

    complete hornnode spec

--- arva-heresy/src/main/java/com/arva/heresy/HornParserConfig.java
+++ arva-heresy/src/main/java/com/arva/heresy/HornParserConfig.java
@@ -3,28 +3,18 @@ public class HornParserConfig{
         String[][] config = {
             {"*, ", "heading"},
             {"TAB,-, ", "unorderedList"},
+            {"TAB,digit,., ", "unorderedList"},
+            {"TAB,letter,., ", "unorderedList"},
+            {"#,+,b,e,g,i,n,_,any, ", "startStructureTemplate"},
+            {"#,+,e,n,d,_,any, ", "endStructureTemplate"},
+            {"#,+,any,:, ", "orgDirective"},
+            // note: Tables are not orgNodes
+            {"|,tableText,|", "table"},
+            {"|,dash,|", "tableSeparator"},
+            {"-,-,-,-,-", "horizontalRule"},
+            {":, ", "orgCode"},
             {"[,f,n,:,any,], ", "footNote"},
-            {"#,+,b,e,g,i,n,_", "startDirective"},
-            {"#,+,e,n,d,_", "endDirective"},
-            /*
-              For reference, this is the regexp HORN-ts uses 
-                heading: /^\*+\s/,
-                list: /^\s*-\s/,
-                nList: /^\s*([A-Za-z]|[0-9]+)\.\s/,
-                sTemplate: /^#\+begin_/,
-                sTemplateEnd: /^#\+end_/,
-                //bSrc: /^#\+begin_src/,
-                //eSrc: /^#\+end_src/,
-                nSrc: /^#\+name:/,
-                table: /^\|([^\|]*\|)+$/,
-                tableSep: /^\|(-+\+)+-+\|$/,
-                empty: /^\s*$/,
-                HR: /^\s*-{5,}\s*$/,
-                orgCode: /^:\s/,
-                footNote: /^\[fn:\d+\]\s/,
-                paragraph: /.+/,
-                clock: /^CLOCK:\s/,
-            */
+            {"C,L,O,C,K,:, ", "clock"},
         };
     }
 }
