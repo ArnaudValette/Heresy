f2f8debb21ec8736eb2ad65f48e9db3c92a05ed6
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-31 10:03:15 +0100

    add dummy implementation of HornParser

--- arva-heresy/src/main/java/com/arva/heresy/HornParser.java
+++ arva-heresy/src/main/java/com/arva/heresy/HornParser.java
@@ -1,7 +1,11 @@
 package com.arva.heresy;
 
 class HornResult {
-    public HornResult() {
+    HornNodes horns;
+    int lineNumber;
+    public HornResult(HornNodes n, int i) {
+        this.horns = n;
+        this.lineNumber = i;
     }
 }
 public class HornParser extends TreeBasedParser {
@@ -11,8 +15,20 @@ public class HornParser extends TreeBasedParser {
 
 
     public HornResult parseHorns(String s, int index) {
-        //System.out.println(s);
+        pNodes<HornNode> horns = new HornNodes();
+        HornParserState state = new HornParserState((HornParserConfig) this.config, horns);
+        treeParse(s, index, state, horns);
+        return new HornResult((HornNodes) horns, index);
+    }
 
-        return new HornResult();
+    static class HornParserState extends TreeBasedParserState {
+        public HornParserState(HornParserConfig config, pNodes<? extends pNode> n) {
+            super(config, n);
+        }
+
+        public void handleTail(int i, Tail t) {
+            nodes.end(i, t.getType(), memory.toString());
+            nodes.commit();
+        }
     }
 }
