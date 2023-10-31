9ad2e932410997382de88a5009841fa32a6161f4
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-31 17:29:04 +0100

    light benchmark

--- arva-heresy/src/main/java/com/arva/heresy/App.java
+++ arva-heresy/src/main/java/com/arva/heresy/App.java
@@ -9,9 +9,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        long startTime = System.currentTimeMillis();
         Parser parser = new Parser();
+        // for(int i = 0; i<20; i++){
+        //     parser.parse(new File("/home/truite/journal/test3.org"));
+        // }
+
+        Runtime runtime = Runtime.getRuntime();
+        long usedMemB = runtime.totalMemory() - runtime.freeMemory();
+        long startTime = System.currentTimeMillis();
         parser.parse(new File("/home/truite/journal/test3.org"));
+        long usedMemE = runtime.totalMemory() - runtime.freeMemory();
+        long memUse = usedMemE - usedMemB;
+        double memoryUsedInKB = (double) memUse / 1024;
+        double memoryUsedInMB = memoryUsedInKB / 1024;
+        System.out.println("Memory Used (Bytes): " + memUse);
+        System.out.println("Memory Used (KB): " + memoryUsedInKB);
+        System.out.println("Memory Used (MB): " + memoryUsedInMB);
         long endTime = System.currentTimeMillis();
         System.out.println("Total execution time: " + (endTime - startTime) + " ms");
         //FileProcessor.iterativeCall("/home/truite/journal/journal.org", bracketParser::parse);
