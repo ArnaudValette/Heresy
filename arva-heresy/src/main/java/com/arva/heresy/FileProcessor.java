4867006396168d5380142e64f9fa546a41bac711
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-11-06 10:37:32 +0100

    simplify Parser.parse, File reading logic extracted to FileProcessor

--- arva-heresy/src/main/java/com/arva/heresy/FileProcessor.java
+++ arva-heresy/src/main/java/com/arva/heresy/FileProcessor.java
@@ -1,16 +1,13 @@
 package com.arva.heresy;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @FunctionalInterface
@@ -23,36 +20,30 @@ interface FormatFunction {
     FormatResult apply(String s, int i);
 }
 
+@FunctionalInterface
+interface FileProcessorCallback {
+    HornResult apply(String s, int i);
+}
+
 public class FileProcessor {
 
-    public static void iterativeCall(String pathName, BracketFunction f) {
-        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        List<Future<BracketResult>> futures = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
+    public static void getHornResults(File file, List<CompletableFuture<HornResult>> list, ExecutorService executor, FileProcessorCallback callBack) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-            AtomicInteger lineNumber = new AtomicInteger(1);
+            AtomicInteger ln = new AtomicInteger(1);
             while ((line = reader.readLine()) != null) {
                 final String lineToProcess = line;
-                final int currentLineNumber = lineNumber.getAndIncrement();
-                futures.add(executor.submit(() -> f.apply(lineToProcess, currentLineNumber)));
-            }
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        } finally {
-            executor.shutdown();
-        }
-
-        List<BracketResult> results = new ArrayList<>();
-        for (Future<BracketResult> future : futures) {
-            try {
-                results.add(future.get());
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+                final int currLn = ln.getAndIncrement();
+                list.add(CompletableFuture.supplyAsync(()->{
+                    return callBack.apply(lineToProcess, currLn);
+                }, executor));
             }
         }
-        results.sort(Comparator.comparingInt(result -> result.lineNumber));
-        results.forEach((res) -> {
-                res.nodes.describe();
-        });
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- arva-heresy/src/main/java/com/arva/heresy/Parser.java
+++ arva-heresy/src/main/java/com/arva/heresy/Parser.java
@@ -1,16 +1,11 @@
 package com.arva.heresy;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Parser {
@@ -22,25 +17,8 @@ public class Parser {
         BracketParser b = new BracketParser();
         FormatParser f = new FormatParser(new FormatParserConfig());
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
-            String line;
-            AtomicInteger ln = new AtomicInteger(1);
-            while((line = reader.readLine()) != null){
-                final String lineToProcess = line;
-                final int currLn = ln.getAndIncrement();
-                CompletableFuture<HornResult> future = CompletableFuture.supplyAsync(()->{
-                    return  parser.parseHorns(lineToProcess, currLn, b, f);
-                    }
-                ,executor);
-                futures.add(future);
-            }
-        }
-        catch(FileNotFoundException e){
-            System.err.println("File " + file.toString() + " not found");
-        }
-        catch(IOException e){
-            System.err.println(e);
-        }
+        // Store futures<HornResult> in futures
+        FileProcessor.getHornResults(file, futures, executor, (s, i)->parser.parseHorns(s, i, b, f));
 
         CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
         return allOf.thenApply(v -> {
