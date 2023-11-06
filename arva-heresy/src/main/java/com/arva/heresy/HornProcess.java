0b2a9c502dc21df5eab5d6d80c18e95cb1f75fe0
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-11-06 10:50:01 +0100

    fileProcessor is more generic, it handles futures operations

--- arva-heresy/src/main/java/com/arva/heresy/HornProcess.java
+++ arva-heresy/src/main/java/com/arva/heresy/HornProcess.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @FunctionalInterface
 interface BracketFunction {
@@ -25,7 +26,17 @@ interface FileProcessorCallback {
     HornResult apply(String s, int i);
 }
 
-public class FileProcessor {
+public class HornProcess {
+
+    public static CompletableFuture<List<HornResult>> getCompletableFutures(List<CompletableFuture<HornResult>> list, ExecutorService executor ) {
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(list.toArray(new CompletableFuture[0]));
+        return allOf.thenApply(v->{
+                return list.stream()
+                    .map(CompletableFuture::join)
+                    .collect(Collectors.toList());
+            }).exceptionally(e->{e.printStackTrace(); return null;})
+                .whenComplete((res, ex) -> executor.shutdown());
+    }
 
     public static void getHornResults(File file, List<CompletableFuture<HornResult>> list, ExecutorService executor, FileProcessorCallback callBack) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
